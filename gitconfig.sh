6d773f84ac13851437e5218f3f93c416987b1493
DarkKnight6499 <yazad.madan@gmail.com>
2017-02-16 18:46:24 +0000

    Add @kishanpatel1998 halfly :p

--- gitconfig.sh
+++ gitconfig.sh
@@ -1,4 +1,4 @@
-OPTIONS="Ga9 Yaz Ak"
+OPTIONS="Ga9 Yaz Ak Kish"
     select opt in $OPTIONS; do
            if [ "$opt" = "Ga9" ]; then
            source identity/GreenArrow9.sh
@@ -8,6 +8,9 @@ OPTIONS="Ga9 Yaz Ak"
            break;
            elif [ "$opt" = "Ak" ]; then
            source identity/AkAbhishek.sh
+           break;
+           elif [ "$opt" = "Kish" ]; then
+           source identity/kishanpatel1998.sh
            break;
            else
            clear
---
+++ identity/kishanpatel1998.sh
@@ -0,0 +1,3 @@
+git config --global user.name "kishpatel1998"
+git config --global user.email ""
+
